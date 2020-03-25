ad78d70521f62457e4ca760d8bacf390e32b04fd
hodgyalice <50991954+hodgyalice@users.noreply.github.com>
2020-03-25 18:49:51 +0100

    test are changed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
-// 1. Сверстать список вариантов
-// 2. Проставить в каждом пункте input[type=radio]
-// 3. Собрать табличку (массив) с баллами за те или иние ответы
-// 4. Написать скрипт, который переберет все отмеченные инпуты, подсчитает итоговое кол-во баллов, и выводит результат.
+
 
 // let array = [];
 // console.log(array)
@@ -21,20 +18,20 @@
 // 	arrayOne.push(mc1.value);
 // })
 
-// const anchors = document.querySelectorAll('a[href*="#"]')
+const anchors = document.querySelectorAll('a[href*="#"]')
 
-// for (let anchor of anchors) {
-//   anchor.addEventListener('click', function (e) {
-//     e.preventDefault()
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
     
-//     const blockID = anchor.getAttribute('href').substr(1)
+    const blockID = anchor.getAttribute('href').substr(1)
     
-//     document.getElementById(blockID).scrollIntoView({
-//       behavior: 'smooth',
-//       block: 'start'
-//     })
-//   })
-// }
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
 
 
 
@@ -53,57 +50,30 @@ $('#results').click(function(){
   var result = 0;
   for(i=0; i<arr.length; i++) {
     result= Number(result)+Number(arr[i]);
-    return result;
-  }  
+    console.log(result);
+  } 
 
-  if(result < 50) {
-  var first = document.getElementById('firstAnsw');
-  first.classList.remove('answer');
-  } else if(50<result<200) {
+   if(1< result && result < 7) {
+    var first = document.getElementById('firstAnsw');
+    firstAnsw.classList.remove('answer');
+    } 
+    else if(7 < result && result < 14) {
     var second = document.getElementById('secondAnsw');
-    second.classList.remove('answer');
-  } else {
+    secondAnsw.classList.remove('answer');
+    } 
+    else if(14 < result){
       var third = document.getElementById('thirdAnsw');
-      third.classList.remove('answer');
-      
-  }
-})
-   
-
+      thirdAnsw.classList.remove('answer');    
+        } 
+    else  {
+      var fourthAnsw = document.getElementById('fourthAnsw');
+      fourthAnsw.classList.remove('answer');
+    }
  
+})
 
 
-// var arr = [];
-// mainSection = document.getElementById('#mainSection');
-// input = document.mainSection.getElementsByTagName('input');
-// btnNext = document.mainSection.getElementsByClassName('btnNext');
-// btnNext.addEventListener('click', function(){
 
-// })
-
-
-// function check() {
-// 	mainSection = document.getElementById('mainSection');
-// 	var count = 0;
-// 	var input = document.mainSection.getElementsByTagName('input').value
-// }
-// var mc1 = document.getElementById('mc1');
-// 	var mainSection = document.getElementById('mainSection');
-//     var inputItem = mainSection.getElementsByTagName('input');
-// function check() { 
-//             for(i = 0; i < input.length; i++) { 
-//                 if(input[i].value.checked==true)  {
-//                   alert('dgraerg')
-//                 	var newDiv = document.createElement('div');
-// 					         newDiv.id = 'true';
-// 					         newDiv.style.backgroundColor = 'green';
-// 					         newDiv.style.color = 'white';
-// 					         newDiv.innerHTML = 'Верно! Вы набрали 20 очков.';
-// 					         mainSection.insertBefore(newDiv, mc1)
-//                 }
-                
-//             } 
-//         } 
 
 
 
